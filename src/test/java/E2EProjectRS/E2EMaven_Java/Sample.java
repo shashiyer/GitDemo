e507af5c3fb540d2d76c771d02351cb8301dd2e3
shashiyer <shash.iyer@gmail.com>
2022-07-11 19:48:52 +0530

    American guy committed

--- src/test/java/E2EProjectRS/E2EMaven_Java/Sample.java
+++ src/test/java/E2EProjectRS/E2EMaven_Java/Sample.java
@@ -5,6 +5,8 @@ public class Sample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Comment added");
+		System.out.println("New comment");
+		System.out.println("New comment 1");
 	}
 
 }
