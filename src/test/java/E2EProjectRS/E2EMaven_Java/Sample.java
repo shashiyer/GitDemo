04a04373c28b55f271c1396cc9c4d5b7919bfaa0
shashiyer <shash.iyer@gmail.com>
2022-07-11 20:21:57 +0530

    Architect Development

--- src/test/java/E2EProjectRS/E2EMaven_Java/Sample.java
+++ src/test/java/E2EProjectRS/E2EMaven_Java/Sample.java
@@ -5,6 +5,14 @@ public class Sample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Comment added");
+		System.out.println("New comment");
+		selenium();
 	}
+	
+	public static void selenium()
+	{
+		System.out.println("Selenium");
+	}
+	
 
 }
